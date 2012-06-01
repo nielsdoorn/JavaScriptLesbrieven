5014aacc70d87f92449402dcb5d020bc9fa94981
Niels Doorn <nielsdoorn@gmail.com>
2012-06-01 08:01:43 +0200

    minimaal voorbeeld met alleen een gameloop
    init en initanimation vanuit query doc ready, draw met een rood blokje

---
+++ lesbrief3/code/minimal-script.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+
+// variabelen
+var canvas;
+
+function init() {	
+	console.log("init");	
+	// canvas met het id "game" opvragen uit HTML
+	canvas = document.getElementById("game");
+}
+
+function initAnimation() {
+	// animatie
+	// vraag aan de browser om maximaal 60 fps te animeren
+	window.requestAnimFrame = (function(callback){
+		return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function(callback){
+			window.setTimeout(callback, 1000 / 60);
+		};
+	})();
+	
+	(function animloop(){
+		requestAnimFrame(animloop);
+
+		tekenScherm();
+	})();
+}
+
+
+// het tekenen van het scherm
+function tekenScherm() {
+	var ctx = canvas.getContext("2d");
+ 	// canvas leeg maken, het canvas is 800px breed en 640px hoog
+	ctx.clearRect(0, 0, 800, 640);
+	draw(ctx);
+}
+
+function draw(ctx) {
+	ctx.fillStyle='red';
+    ctx.strokeStyle='red';
+	ctx.fillRect(100,100,50,50);
+}
